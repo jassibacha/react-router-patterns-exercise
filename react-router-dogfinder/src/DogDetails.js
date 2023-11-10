e8e39a1bdf48b0f0d9ebec5b53df3afeb8a0579e
Jassi Bacha <klikster@gmail.com>
2023-11-10 15:13:06 -0800

    Add check for if params

--- react-router-dogfinder/src/DogDetails.js
+++ react-router-dogfinder/src/DogDetails.js
@@ -1,9 +1,28 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
 
-function DogDetails() {
+function DogDetails({ dogs }) {
     const { name } = useParams();
-    return <div>Details: {name}</div>;
+
+    if (name) {
+        const dog = dogs.find(
+            (d) => d.name.toLowerCase() === name.toLowerCase()
+        );
+        return (
+            <div>
+                <h1>{dog.name}</h1>
+                <p>Age: {dog.age}</p>
+                Facts:
+                <ul>
+                    {dog.facts.map((fact, i) => (
+                        <li key={i}>{fact}</li>
+                    ))}
+                </ul>
+            </div>
+        );
+    }
+
+    return null;
 }
 
 export default DogDetails;
